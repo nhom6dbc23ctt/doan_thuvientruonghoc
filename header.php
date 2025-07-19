ab5ed78aad8e8923edd63fe4f60d7a327d680e32
nhom6dbc23ctt <235ctt002_len@pyu.edu.vn>
2025-07-19 14:59:05 +0700

    Update source + SQL mẫu

--- header.php
+++ header.php
@@ -26,13 +26,13 @@
 <?php if(isset($_SESSION['user'])): ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
   <div class="container">
-    <a class="navbar-brand" href="dashboard.php">📚 Thư viện</a>
+    <a class="navbar-brand" href="dashboard.php">📚 Thư viện trường học số</a>
     <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
       <span class="navbar-toggler-icon"></span>
     </button>
     <div class="collapse navbar-collapse" id="navbarNav">
       <ul class="navbar-nav ms-auto">
-          <li class="nav-item"><a class="nav-link" href="dashboard.php">🏠 Dashboard</a></li>
+          <li class="nav-item"><a class="nav-link" href="dashboard.php">🏠 Home</a></li>
           <li class="nav-item"><a class="nav-link" href="borrow.php">✅ Mượn sách</a></li>
           <li class="nav-item"><a class="nav-link" href="return.php">↩️ Trả sách</a></li>
           <?php if ($_SESSION['user']['role'] != 'user'): ?>
